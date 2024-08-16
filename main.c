ccc8da7308f9996173ee85fd6ea4d9efe9ba95e0
jesuserr <ivoice666@hotmail.com>
2024-08-17 01:23:24 +0200

    Modify main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 extern size_t   ft_strlen(const char *str);
 extern char     *ft_strcpy(char *dst, const char *src);
 extern int      ft_strcmp(const char *s1, const char *s2);
-extern ssize_t  write(int fd, const void *buf, size_t count);
+extern ssize_t  ft_write(int fd, const void *buf, size_t count);
 
 int main() {
     const char *str = "Hello, World!";
@@ -22,7 +22,9 @@ int main() {
     const char *str4 = "ABJ";
     const char *str5 = "ABC";
     printf("%d %d\n", strcmp(str4,str5), ft_strcmp(str4,str5));
-    printf("%d\n", write(1, "Hello, World!\n", 14));
-    printf("%d\n", ft_write(1, "Hello, World!\n", 14));
+    printf("%ld\n", write(1, "Hello, World!\n", 14));
+    printf("%ld\n", ft_write(1, "Hello, World!\n", 14));
     return 0;
 }
+
+// make && gcc -Wall -Werror -Wextra main.c libasm.a && ./a.out
