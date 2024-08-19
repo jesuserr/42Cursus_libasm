7242a4745d23d4d844ad34c0077716d3589bedaf
jesuserr <ivoice666@hotmail.com>
2024-08-20 00:08:34 +0200

    libasm.h creation

---
+++ libasm.h
@@ -0,0 +1,31 @@
+#ifndef LIBASM_H
+# define LIBASM_H
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              HEADERS
+*/
+# include <unistd.h>    		// for size_t and ssize_t
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              STRUCT DECLARATIONS
+*/
+typedef struct s_list
+{
+	void			*data;
+	struct	s_list 	*next;
+}	t_list;
+
+/*
+** -.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-.-'-
+**                              FUNCTION PROTOTYPES
+*/
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s);
+
+#endif
--- tests.c
+++ tests.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <errno.h>
-
-// Declare the external function
-extern size_t   ft_strlen(const char *str);
-extern char     *ft_strcpy(char *dst, const char *src);
-extern int      ft_strcmp(const char *s1, const char *s2);
-extern ssize_t  ft_write(int fd, const void *buf, size_t count);
-extern ssize_t  ft_read(int fd, void *buf, size_t count);
-extern char     *ft_strdup(const char *s);
+#include "libasm.h"
 
 int main() {
     const char *str = "Hello, World!";
@@ -60,4 +52,4 @@ int main() {
     return 0;
 }
 
-// make && gcc -Wall -Werror -Wextra main.c libasm.a && ./a.out
+// make && gcc -Wall -Werror -Wextra tests.c libasm.a && ./a.out
