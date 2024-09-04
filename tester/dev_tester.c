4a82ced91ca9dd3eadfa1c6a83f2cfb34e6763dc
Jesus <ivoice666@hotmail.com>
2024-09-04 22:07:41 +0200

    Add operations with files for ft_write and ft_read on dev_tester.c

--- tester/dev_tester.c
+++ tester/dev_tester.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <errno.h>
+#include <fcntl.h>
 #include "../libasm/libasm.h"
 
 #define LONG_SIZE 100000000             // 100 MBytes
@@ -10,6 +11,7 @@ int	main(void)
 {
 	printf("\033[0;32mTest ft_strlen\033[0m\n");
 	const char *str = "Hello, World!";
+    printf("%s\n", str);
 	printf("Standard function -> Length: %zu\n", strlen(str));
 	printf("Assembly function -> Length: %zu\n\n", ft_strlen(str));
 
@@ -26,7 +28,7 @@ int	main(void)
     printf("Standard function -> String: %s\n", strcpy(str2, str));
     printf("Standard function -> Length: %zu\n", strlen(str2));
     printf("Assembly function -> String: %s\n", ft_strcpy(str3, str));
-    printf("Standard function -> Length: %zu\n", ft_strlen(str3));
+    printf("Assembly function -> Length: %zu\n", ft_strlen(str3));
     printf("Assembly function -> Difference: %d \n\n", ft_strcmp(str2, str3));
 
     printf("\033[0;32mTest ft_strcpy (very long 100 MBytes)\033[0m\n");
@@ -52,18 +54,45 @@ int	main(void)
     printf("Assembly function -> Error number: %d\n", errno);
     printf("Assembly function -> Error message: %s\n\n", strerror(errno));
 
+    printf("\033[0;32mTest ft_write (to a file)\033[0m\n");
+    int fd = open("file_test", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    printf("Standard function -> Printed: %ld\n", write(fd, "Standard Hello, World! ", 23));
+    printf("Standard function -> Error number: %d\n", errno);
+    printf("Standard function -> Error message: %s\n", strerror(errno));
+    printf("Assembly function -> Printed: %ld\n", ft_write(fd, "Assembler Hello, World!", 23));
+    printf("Assembly function -> Error number: %d\n", errno);
+    printf("Assembly function -> Error message: %s\n\n", strerror(errno));
+    close(fd);
+
     printf("\033[0;32mTest ft_read\033[0m\n");
     char *str6 = calloc(100, sizeof(char));
     char *str7 = calloc(100, sizeof(char));
-    printf("Reading...\n");
+    printf("Enter a text for comparison:\n");
     printf("Standard function -> Read: %ld\n", read(1, str6, 100));
     printf("Standard function -> Error number: %d\n", errno);
     printf("Standard function -> Error message: %s\n", strerror(errno));
-    printf("Reading...\n");
-    printf("Standard function -> Read: %ld\n", ft_read(1, str7, 100));
+    printf("Enter a text for comparison:\n");
+    printf("Assembly function -> Read: %ld\n", ft_read(1, str7, 100));
     printf("Assembly function -> Error number: %d\n", errno);
     printf("Assembly function -> Error message: %s\n", strerror(errno));
-    printf("Comparison: %s\n\n", ft_strcmp(str6, str7) ? "Different" : "Equal");
+    printf("Assembly function -> Difference: %d \n\n", ft_strcmp(str6, str7));
+
+    printf("\033[0;32mTest ft_read (from a file)\033[0m\n");
+    int fd2 = open("file_test", O_RDONLY);
+    char *str15 = calloc(100, sizeof(char));    
+    printf("Standard function -> Read: %ld\n", read(fd2, str15, 100));
+    printf("Standard function -> String: %s\n", str15);
+    printf("Standard function -> Error number: %d\n", errno);
+    printf("Standard function -> Error message: %s\n", strerror(errno));
+    close(fd2);
+    int fd3 = open("file_test", O_RDONLY);
+    char *str16 = calloc(100, sizeof(char));
+    printf("Assembly function -> Read: %ld\n", ft_read(fd3, str16, 100));
+    printf("Assembly function -> String: %s\n", str16);
+    printf("Assembly function -> Error number: %d\n", errno);
+    printf("Assembly function -> Error message: %s\n", strerror(errno));
+    close(fd3);
+    printf("Assembly function -> Difference: %d \n\n", ft_strcmp(str15, str16));
 
     printf("\033[0;32mTest ft_strdup\033[0m\n");
     char *str8 = strdup("Hello, World!");
@@ -94,9 +123,9 @@ int	main(void)
     free(str12);
     free(str13);
     free(str14);
+    free(str15);
+    free(str16);
     return 0;
 }
 // make && gcc -Wall -Werror -Wextra dev_tester.c ../libasm/libasm.a && ./a.out
-
-// write with file open
-// read with open file
+// make && gcc -Wall -Werror -Wextra dev_tester.c ../libasm/libasm.a && valgrind --leak-check=full ./a.out
